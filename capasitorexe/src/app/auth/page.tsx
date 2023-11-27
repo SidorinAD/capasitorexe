b62ac7988a506d51ac8d9a1dac2dd19efb220458
SidorinAD <57830895+SidorinAD@users.noreply.github.com>
2023-11-27 16:25:01 +0300

    first test build

---
+++ capasitorexe/src/app/auth/page.tsx
@@ -0,0 +1,37 @@
+import { Capacitor } from "@capacitor/core";
+import styles from "./page.module.css";
+
+const Auth = () => {
+  if (Capacitor.getPlatform() === "web") {
+  } else if (Capacitor.getPlatform() === "ios") {
+  } else if (Capacitor.getPlatform() === "android") {
+  }
+
+  return (
+    <div className={styles.authContainer}>
+      <p className={styles.authTitle}>
+        Для использования данного приложения необходимо авторизоваться
+      </p>
+
+      <p className={styles.authTitle}>
+        {`Capacitor.getPlatform() === "web" ${
+          Capacitor.getPlatform() === "web"
+        }`}
+      </p>
+
+      <p className={styles.authTitle}>
+        {`Capacitor.getPlatform() === "ios" ${
+          Capacitor.getPlatform() === "ios"
+        }`}
+      </p>
+
+      <p className={styles.authTitle}>
+        {`Capacitor.getPlatform() === "androud" ${
+          Capacitor.getPlatform() === "androud"
+        }`}
+      </p>
+    </div>
+  );
+};
+
+export default Auth;
