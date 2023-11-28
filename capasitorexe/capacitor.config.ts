9f28179d0d03afff3d5a227d109b41d4bad97362
SidorinAD <57830895+SidorinAD@users.noreply.github.com>
2023-11-28 11:42:07 +0300

    capacitor config build fix

--- capasitorexe/capacitor.config.ts
+++ capasitorexe/capacitor.config.ts
@@ -3,10 +3,10 @@ import { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "com.capasitor.app",
   appName: "capasitorexe",
-  webDir: "out",
-  server: {
-    url: "192.168.0.137:3000"
-  }
+  webDir: "out"
+  // server: {
+  //   url: "192.168.0.137:3000"
+  // }
 };
 
 export default config;
